6e17304df71252817a1013eebfde787b10d82d83
MarvinHei <marvin.heidinger@t-online.de>
2024-06-20 15:26:53 +0200

    Bug fix with folder structure

--- demo.py
+++ demo.py
@@ -14,7 +14,16 @@ output_resolution= (854,480)
 is_overlay=False
 rgb_frames = 'epick_visor/GroundTruth-SparseAnnotations/rgb_frames/P02'
 generate_video=False
-side = "left"
-type = "object"
+sides = ["left", "right"]
+types = ["object", "hand"]
 
-folder_of_jsons_to_masks(json_files_path, output_directory, side, type, is_overlay=is_overlay,rgb_frames=rgb_frames,output_resolution=output_resolution,generate_video=generate_video)
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+for side in sides:
+    for type in types:
+        folder_of_jsons_to_masks(json_files_path, output_directory, side, type, is_overlay=is_overlay,rgb_frames=rgb_frames,output_resolution=output_resolution,generate_video=generate_video)
+
+folder_of_jsons_to_masks(json_files_path, output_directory, "both", "hand", is_overlay=is_overlay,rgb_frames=rgb_frames,output_resolution=output_resolution,generate_video=generate_video)
+
+
